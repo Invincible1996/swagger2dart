e37d1ef9ca9146a8043f2e0c1a9ebed69d028c55
张文强 <wenqiang.zhang@visionacademy.cn>
2022-07-14 17:40:31 +0800

    first commit

---
+++ bin/swagger2dart.dart
@@ -0,0 +1,5 @@
+import 'package:swagger2dart/swagger2dart.dart' as swagger2dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${swagger2dart.calculate()}!');
+}
---
+++ test/swagger2dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:swagger2dart/swagger2dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
